bc5e8c7b3d599470b112d480b1fed7e48142ee5c
Vishal R <vishalravichandran1@gmail.com>
2026-01-09 23:35:33 +0530

    Remove app background logic from app.py

--- app.py
+++ app.py
@@ -21,43 +21,6 @@ st.set_page_config(
     layout="wide"
 )
 
-def get_base64(bin_file):
-    with open(bin_file, 'rb') as f:
-        data = f.read()
-    return base64.b64encode(data).decode()
-
-
-def set_background(png_file):
-    bin_str = get_base64(png_file)
-    page_bg_img = '''
-    <style>
-    .stApp {
-    background-image:
-        linear-gradient(rgba(255, 255, 255, 0.85), rgba(255, 255, 255, 0.85)),
-        url("data:image/png;base64,%s");
-    background-size: cover;
-    }
-    </style>
-    ''' % bin_str
-    st.markdown(page_bg_img, unsafe_allow_html=True)
-
-set_background('resources/sky_background.jpg')
-
-
-page_bg_img = '''
-<style>
-.stApp {
-    background-image:
-        linear-gradient(rgba(255, 255, 255, 0.85), rgba(255, 255, 255, 0.85)),
-        url("https://unsplash.com/photos/a-view-of-a-mountain-range-with-snow-capped-mountains-in-the-background-sSqbZ9zI86E");
-    background-size: cover;
-}
-</style>
-'''
-
-
-st.markdown(page_bg_img, unsafe_allow_html=True)
-
 # --------------------------------------------------
 # MODEL METADATA
 # --------------------------------------------------
