ea82f0935c2b9e26c1eb0158b5917114d6b43939
Vishal R <vishalravichandran1@gmail.com>
2026-01-09 23:32:24 +0530

    Modify the logic to add app background based on local background image

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import pickle
 import seaborn as sns
 import matplotlib.pyplot as plt
 import numpy as np
+import base64
 
 from sklearn.metrics import (
     accuracy_score, precision_score, recall_score, f1_score,
@@ -19,6 +20,29 @@ st.set_page_config(
     page_title="ML Classification Model Evaluator",
     layout="wide"
 )
+
+def get_base64(bin_file):
+    with open(bin_file, 'rb') as f:
+        data = f.read()
+    return base64.b64encode(data).decode()
+
+
+def set_background(png_file):
+    bin_str = get_base64(png_file)
+    page_bg_img = '''
+    <style>
+    .stApp {
+    background-image:
+        linear-gradient(rgba(255, 255, 255, 0.85), rgba(255, 255, 255, 0.85)),
+        url("data:image/png;base64,%s");
+    background-size: cover;
+    }
+    </style>
+    ''' % bin_str
+    st.markdown(page_bg_img, unsafe_allow_html=True)
+
+set_background('resources/sky_background.jpg')
+
 
 page_bg_img = '''
 <style>
