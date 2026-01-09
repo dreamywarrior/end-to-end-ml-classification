84f572866c66f0b4410e3d5bc6a2b8e3dbbf3911
Vishal R <vishalravichandran1@gmail.com>
2026-01-09 14:29:54 +0530

    Update AUC calculation to support multi-class ROC AUC and specify scikit-learn version in requirements

--- app.py
+++ app.py
@@ -55,7 +55,7 @@ if uploaded_file:
 
     try:
         y_prob = model.predict_proba(X_test)[:, 1]
-        auc = roc_auc_score(y_test_encoded, y_prob)
+        auc = roc_auc_score(y_test_encoded, y_prob, multi_class='ovr')
     except:
         auc = "N/A"
 
